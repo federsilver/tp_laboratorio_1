cf955844a6c9e687c310f2247a378629a4f6e236
Federico Silvero <52494803+federsilver@users.noreply.github.com>
2019-09-12 13:19:24 -0300

    Calculadora funcionando

--- main.c
+++ main.c
@@ -1,48 +1,32 @@
-
-
 #include <stdio.h>
+#include <stdlib.h>
 
-int main()
-{
-    float num1;
-    char operador;
-    float num2;
-    float resultado;
+int main() {
 
-    printf("Ingrese [numero 1]\n");
-    scanf("%f", &num1);
+double numero1;
+double numero2;
+char ope;
+double resultado;
 
-    printf("Ingrese [+ - * /]\n");
-    scanf("%c", &operador);
+printf("Ingresa primer numero: \n");
+scanf("%lf", &numero1);
+printf("Ingresa operacion: \n");
+scanf(" %c", &ope);
+printf("Ingresa segundo numero: \n");
+scanf("%lf", &numero2);
 
-    printf("Ingrese [numero 2]\n");
-    scanf("%f", &num2);
-
-    /* Switch the value and perform action based on operator*/
-    switch(operador)
-    {
-        case '+':
-            resultado = num1 + num2;
-            break;
-
-        case '-':
-            resultado = num1 - num2;
-            break;
-
-        case '*':
-            resultado = num1 * num2;
-            break;
-
-        case '/':
-            resultado = num1 / num2;
-            break;
-
-        default:
-            printf("Error");
+    if (ope == '+') {
+        resultado = numero1 + numero2;
+    } else if (ope == '-') {
+        resultado = numero1 - numero2;
+    } else if (ope == '*') {
+        resultado = numero1 * numero2;
+    } else if (ope == '/') {
+        resultado = numero1 / numero2;
+    } else {
+        printf("Error en el ingreso");
     }
 
-    /* Detalla el resultado */
-    printf("%.2f %c %.2f = %.2f", num1, operador, num2, resultado);
+return resultado;
 
-    return 0;
 }
